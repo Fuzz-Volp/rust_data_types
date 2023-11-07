abdc458938833f9264a1a80671a49f4d80ba3285
John Volpitta <johnvolpitta@gmail.com>
2023-11-06 19:24:48 -0800

    adding characters

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
+
 fn main() {
-    println!("Hello, world!");
+    // float
+    let pi: f32 = 4.0; 
+
+    //number separator
+    let million = 1_000_000;
+
+    println!("{}", million);
+    //boolean
+    let is_day = true;
+
+    let is_night = false;
+
+    println!("{}", is_day);
+    
+    // Characters 
+    let char1 = 'A';
+    println!("{}", char1);
+
+    let smiley_face = '\u{1F601}';
+    println!("{}", smiley_face)
 }
